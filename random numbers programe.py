816c7d5e35c64cdfd667bd4177e387adf86205f3
sumit00000 <76873515+sumit00000@users.noreply.github.com>
2022-10-01 16:12:58 +0530

    Random numbers programs

---
+++ random numbers programe.py
@@ -0,0 +1,12 @@
+# Random Numbers
+
+import random
+
+print(random.choice(range(6)))
+print(random.choice([1, 2, 3, 4, 5, 6]))
+print(random.choice("SumitSingh"))
+
+print(random.randrange(1, 100, 2))
+
+# random.random print number between 0 and 1
+print(random.random())
